fe7ebe22ced0df6fcdd1f4d9d64582208f076d69
Jonathan Norman <jnorman2123@gmail.com>
2019-11-14 03:36:25 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -7,10 +7,13 @@ class Application
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
-      @@items
+      item_name = req.path.split("/items/").last 
+      item = @@items.find {|i| i.name == item_name}
     else
       resp.write "Route not found"
       resp.status = 404
     end
+
+    resp.finish
   end
 end
