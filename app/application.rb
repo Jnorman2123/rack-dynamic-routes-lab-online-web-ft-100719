bc830d874b17d35dbe3b6de6abcb79a05d7d658c
Jonathan Norman <jnorman2123@gmail.com>
2019-11-14 03:29:22 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,15 +2,15 @@ class Application
 
   @@items = []
 
-  def call(env) 
-    resp = Rack::Response.new 
+  def call(env)
+    resp = Rack::Response.new
     req = Rack::Request.new(env)
 
     if req.path.match(/items/)
       @@items
-    else 
+    else
       resp.write "Route not found"
       resp.status = 404
-    end 
-  end 
-end   
+    end
+  end
+end
